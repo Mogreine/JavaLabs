7fc1290e9a16f0ad2bb5da16dd1c6a4358b8f44e
Mogreine <north7311@gmail.com>
2017-10-18 16:01:53 +0400

    test

--- src/main/MaxRightElement.java
+++ src/main/MaxRightElement.java
@@ -3,9 +3,11 @@ package main;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
-
+import java.util.stream.Stream;
 
 
 public class MaxRightElement {
