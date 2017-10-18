a5ddfaf29cc7b213104d2c7d5574420b61756a66
Admin <north7311@gmail.com>
2017-10-18 16:26:02 +0400

    Начал очередь

---
+++ src/main/Queue.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Queue {
+    public static class myQueue {
+        private int size;
+        private LinkedList<Integer> elements;
+
+        myQueue() {
+            size = 0;
+            elements = new LinkedList<>();
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public void push(int value) {
+
+        }
+    }
+}
