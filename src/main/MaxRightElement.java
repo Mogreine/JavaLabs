644963fa7915618aa0f03d7d02ee092205bc79a7
Admin <north7311@gmail.com>
2017-10-20 15:19:59 +0400

    Time Limit 25 тест, надо думать

--- src/main/MaxRightElement.java
+++ src/main/MaxRightElement.java
@@ -3,28 +3,11 @@ package main;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
-import java.util.stream.Stream;
 
 
 public class MaxRightElement {
-
-    public static class pair {
-        int value;
-        int index;
-
-        pair() {
-            this(0, 0);
-        }
-
-        pair(int value, int index) {
-            this.value = value;
-            this.index = index;
-        }
-    }
 
     public static void main(String[] args) {
         int     x,
@@ -44,41 +27,38 @@ public class MaxRightElement {
         startElement = Integer.parseInt(separatedData.nextToken());
         x = Integer.parseInt(separatedData.nextToken());
         y = Integer.parseInt(separatedData.nextToken());
-        pair[] arr = fillArray(amount, startElement, x, y);
-        int sum = getIndexSum(arr);
+        int[] arr = fillArray(amount, startElement, x, y);
+        long sum = getIndexSum(arr);
         System.out.println(sum);
     }
 
-    static pair[] fillArray(int amount, int startElement, int x, int y) {
-        pair[] arr = new pair[amount];
-        arr[0] = new pair(startElement, 0);
+    static int[] fillArray(int amount, int startElement, int x, int y) {
+        int[] arr = new int[amount];
+        arr[0] = startElement;
         for (int i = 1; i < arr.length; i++) {
-            long tempLong = ((long) arr[i - 1].value * x + y) % 1000000007;
-            arr[i] = new pair((int) tempLong, i);
+            arr[i] = (int) (((long) arr[i - 1] * x + y) % 1000000007);
         }
         return arr;
     }
 
-    static int getIndexSum(pair[] array) {
-        Stack<pair> sequence = new Stack<>();
-        int[] indexArray = new int[array.length];
+    static long getIndexSum(int[] array) {
+        Stack<Integer> sequence = new Stack<>();
+        long sum = 0;
         for (int i = 0; i < array.length; i++) {
-            if (sequence.isEmpty() || array[i].value <= sequence.peek().value) {
+            if (sequence.isEmpty() || array[i] <= sequence.peek()) {
                 sequence.push(array[i]);
             }
             else {
-                while (!sequence.empty() && sequence.peek().value < array[i].value) {
-                    indexArray[sequence.pop().index] = i;
+                while (!sequence.empty() && sequence.peek() < array[i]) {
+                    sequence.pop();
+                    sum += i;
                 }
                 sequence.push(array[i]);
             }
         }
         while (!sequence.empty()) {
-            indexArray[sequence.pop().index] = -1;
-        }
-        int sum = 0;
-        for (int i = 0; i < indexArray.length; i++) {
-            sum += indexArray[i];
+            sum += -1;
+            sequence.pop();
         }
         return sum;
     }
